d92c0ba690fc002f09c7572ead1237e287e70b02
Askar <Ask4r.pr@gmail.com>
2020-02-09 13:09:52 +0600

    Initial Commit

---
+++ Diploma/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Diploma
+//
+//  Created by Askar Serikkhanov on 2/9/20.
+//  Copyright © 2020 Askar Serikkhanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
